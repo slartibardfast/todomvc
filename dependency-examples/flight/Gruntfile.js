e3756bfc0433e2ea383b185c08a008f2b484b281
David Connolly <david@connolly.name>
2013-08-06 07:35:51 +0100

    Add copy and clean tasks

--- dependency-examples/flight/Gruntfile.js
+++ dependency-examples/flight/Gruntfile.js
@@ -6,6 +6,19 @@ module.exports = function(grunt) {
           options: { "browser" : true
                    }
         }
+    copy : {
+     files : {
+       expand : true,
+       cwd : './',
+       src : [
+         'index.html', 'app/**', 'bower_components/**'
+        ],
+       dest : 'target/build/'
+       }
+     },
+     clean : {
+       files : [ "target/build/", "target/release" ]
+      },
     watch: {
       scripts: {
       files: ['app/js/**/*.js'],
@@ -14,8 +27,10 @@ module.exports = function(grunt) {
     },
   });
  
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'clean', 'copy']);
 };
